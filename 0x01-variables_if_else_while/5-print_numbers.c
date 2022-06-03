6f4ac67e3e13a0da1dada3ab56c9529cfc0c3a9e
Ugoo <ugonnaezeabara@gmail.com>
2022-06-03 13:47:21 -0700

    learning C variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program starting point
+ *
+ * Return: end program
+ *
+*/
+
+int main(void)
+{
+	int base;
+
+	for (base = 0; base <= 10; base++)
+	{
+		printf("%d", base);
+	}
+}
