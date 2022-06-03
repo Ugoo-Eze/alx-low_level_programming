a5866b0149c467cad33fec84718652f02059bd0d
Ugoo <ugonnaezeabara@gmail.com>
2022-06-03 12:22:28 -0700

    learning C variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,12 +11,12 @@ int main(void)
 {
 	char letter;
 
-	for (letter = "a"; letter <= "z"; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
 	}
 
-	putchar("\n");
+	putchar('\n');
 
 	return (0);
 }
