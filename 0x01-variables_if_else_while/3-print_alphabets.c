c364ef4899ddf498c959dac0f5dddf2168e4c26f
Ugoo <ugonnaezeabara@gmail.com>
2022-06-03 12:56:29 -0700

    learning C variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - program starting point
+ *
+ * Return: end program
+ *
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+}
