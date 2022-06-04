1635249d860e99af414934215a68b069c487ae5d
Ugoo <ugonnaezeabara@gmail.com>
2022-06-03 17:14:00 -0700

    larning C variables

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,17 +12,17 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 9; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
-		if (n != 9)
+		if (n != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
 
-	putchar('\n');
+	putchar(10);
 	return (0);
 
 }
