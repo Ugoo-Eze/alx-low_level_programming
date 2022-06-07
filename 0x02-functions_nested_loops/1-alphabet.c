c6671ce3a5f7d43f45f27ef4ed7dc14b699a53bd
Ugoo <ugonnaezeabara@gmail.com>
2022-06-07 06:49:32 -0700

    learning functions in C

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function to print a-z
+ *
+ * Return: end program
+ *
+*/
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha < 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+
+	putchar('\n');
+	return (0);
+}
