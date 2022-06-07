8eb019c7a8e361aa4306f6808f0086ffffdfedf1
Ugoo <ugonnaezeabara@gmail.com>
2022-06-07 14:57:26 -0700

    learning functions in C

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _print_sign - function to print sign
+ *
+ * @n: the character to execute
+ *
+ * Return: return 1 if c is positive
+ * return 0 if 0
+ * return -1 if nagative
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
