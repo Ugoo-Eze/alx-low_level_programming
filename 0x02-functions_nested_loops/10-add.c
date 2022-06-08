c73365cf9569af310d4e63df1ff400983e414345
Ugoo <ugonnaezeabara@gmail.com>
2022-06-08 00:05:35 -0700

    learning functions in C

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function to add two numbers
+ *
+ * @a: argument 1
+ * @b: argument 2
+ *
+ * Return: return sum
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
