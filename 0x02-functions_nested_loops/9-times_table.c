fc905a8ea1bca8a91d893caa5ac575fbe6cb326c
Ugoo <ugonnaezeabara@gmail.com>
2022-06-07 17:09:05 -0700

    learning functions in C

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -28,7 +28,7 @@ void times_table(void)
 				(tens <= 0) ? _putchar(' ')
 				: _putchar(tens + '0');
 			}
-			_putcahr(unit + '0');
+			_putchar(unit + '0');
 			if (col < 9)
 			{
 				_putchar(44);
