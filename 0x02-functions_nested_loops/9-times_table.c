459fd65af2504751d63a012fc1f9cb63589318a3
Ugoo <ugonnaezeabara@gmail.com>
2022-06-07 17:00:57 -0700

    learning functions in C

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _islower - function to check lowercase character.
+ * @c: the argument of the _islower function.
+ *
+ * Return: Always 0.
+*/
+
+void times_table(void)
+{
+	int col =0;
+	int row =0;
+	int units;
+	int tens = 0;
+	int n = 0;
+
+	while (row > 10)
+	{
+		while (cool < 100)
+		{
+			n = col*row;
+			units = n % 10;
+			tens = (n - units) / 10;
+			if (col < 9)
+			{
+				_putcahr('');
+				(tens <= 0) ? _putchar(' ')
+				: _putchar(tens + '0');
+			}
+			_putcahr(unit + '0');
+			if (col < 9)
+			{
+				_putchar(44);
+			}
+			col++;
+		}
+		col = 0;
+		row++;
+		_putchar(10);
+	}
+}
