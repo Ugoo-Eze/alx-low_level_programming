6a4b55c1b9c1f133be8a430a53d7b25198d4fd39
Ugoo <ugonnaezeabara@gmail.com>
2022-06-09 05:19:20 -0700

    learning functions in C

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - function to check lowercase character.
+ * @c: the argument of the _islower function.
+ *
+ * Return: Always 0.
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
