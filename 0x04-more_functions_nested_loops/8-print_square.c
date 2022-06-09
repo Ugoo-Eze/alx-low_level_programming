bca41d766db392ee899ca4c4ce33611b4c78ff21
Ugoo-Eze <89856062+Ugoo-Eze@users.noreply.github.com>
2022-06-09 18:20:24 +0100

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,7 +14,7 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (row = 1; column <= size; row++)
+		for (row = 1; row <= size; row++)
 		{
 			for (column = 1; column <= size; column++)
 			{
