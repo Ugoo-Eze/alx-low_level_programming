9d2afcad23b4358bff1bb1591775362958411c5b
Ugoo <ugonnaezeabara@gmail.com>
2022-06-09 09:02:53 -0700

    learning functions in C

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,25 +1,18 @@
 #include "main.h"
 
 /**
- * more_numbers - function to print digit 1-14x10
+ * print line - function to print straight line
  *
  * Return: Always 0.
 */
 
 void print_line(int n)
 {
-	int lines;
+	int lines = n;
 
-	for (lines = n; lines > 0; lines++)
+	for (lines = n; lines > 0; lines--)
 	{
-		if (lines < 0)
-		{
-			_putchar('\n');
-		}
-
 		_putchar('_');
-		lines++;
 	}
-
 	_putchar('\n');
 }
