96c520f1c1283eb44a315fcb12817681b3b2468e
Ugoo <ugonnaezeabara@gmail.com>
2022-06-09 05:40:05 -0700

    learning functions in C

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function to multiply two numbers
+ * @a: the argument of the mul function.
+ * @b: the argument of the mul function.
+ *
+ * Return: Always 0.
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
