72721a1f2a5fefcebdd79664fbbe045c675653d6
Ugoo <ugonnaezeabara@gmail.com>
2022-06-13 09:59:29 -0700

    learning pointers and arrays in C

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - function to print string
+ *
+ * @str: pointer of parameter
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
