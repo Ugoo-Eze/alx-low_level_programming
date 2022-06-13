6975c342b698ce879beb5f42d8a878b8fe49cdfa
Ugoo <ugonnaezeabara@gmail.com>
2022-06-13 08:47:57 -0700

    learning pointers and arrays in C

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - function to swap two values
+ *@a: argument value 1
+ *@b: argument value 2
+*/
+
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (str[n] != '\0')
+	{
+		n++;
+	}
+
+	return (n);
+}
