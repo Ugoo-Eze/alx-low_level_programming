52f71a4e4a90003279fa90678ac2487ad8df888c
Ugoo <ugonnaezeabara@gmail.com>
2022-06-13 13:21:55 -0700

    learning pointers and arrays in C

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - function to print half of string
+ *
+ * @str: pointer for argument
+*/
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int half;
+
+	while (str[length] != '\0')
+	{
+		length++
+	}
+
+	half = (length % 2 == 0) ? length / 2
+	: (length + 1) / 2;
+
+	while (str[half] != '\0')
+	{
+		_putchar(str[half++]);
+	}
+
+	_putchar('\0');
+}
