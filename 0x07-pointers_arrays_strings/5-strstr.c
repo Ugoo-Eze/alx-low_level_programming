106413cb973b2349d4b1486bc80bd0300ea0dee8
Ugoo <ugonnaezeabara@gmail.com>
2022-06-20 13:08:13 -0700

    learning pointers, arrays & strings in C

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #define NULL 0
 
 /**
@@ -11,5 +11,38 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	int i = 0, j, k;
 
+	if (needle[0] == '\0')
+	{
+		return (haystack);
+	}
+
+	while (haystack[i] != '\0')
+	{
+		if (haystack[i] == needle[0])
+		{
+			k = i, j = 0;
+
+			while (needle[j] != '\0')
+			{
+				if (haystack[k] == needle[j])
+				{
+					k++, j++;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			if (needle[j] == '\0')
+			{
+				return (haystack + 1);
+			}
+		}
+		i++;
+	}
+
+	return (NULL);
 }
