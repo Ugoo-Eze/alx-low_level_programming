5e9d4507647451c975f33bc3b7019da6d89b87cb
Ugoo <ugonnaezeabara@gmail.com>
2022-06-20 06:13:09 -0700

    learning pointers, arrays & strings in C

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#define NULL 0
+
+/**
+ * _strstr - find and return pointer to first occurence of substring
+ * @haystack: string to search
+ * @needle: target substring to search for
+ *
+ * Return: pointer to index of string at first occurence of whole substring
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+
+}
