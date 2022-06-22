e052e60964b5f2bf47b25c900cef64a9623212f0
Ugoo <ugonnaezeabara@gmail.com>
2022-06-22 11:31:47 -0700

    learning recursion in C

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * is_prime -function to check for  prime
+ * @x: number
+ * @y: number
+ *
+ * Return: result
+*/
+
+int is_prime(int n, int divisor)
+{
+	if (n == divisor)
+		return (1);
+	else if (n % divisor == 0)
+		return (0);
+	else
+		return (is_prime(n, divisor + 1));
+}
+
+
+/**
+ * is_prime_number - check if prime
+ * @n: number to check
+ * Return: 1 if prime, 0 if not
+*/
+
+int is_prime_number(int n)
+{
+	int divisor = 3;
+
+	if (n % 2 == 0 || n < 2)
+		return (0);
+	if (n == 2)
+		return (1);
+	return (is_prime(n, divisor));
+}
