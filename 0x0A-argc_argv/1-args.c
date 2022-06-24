5f54809bc9c41e25cb0305e3511112461f919c2a
Ugoo <ugonnaezeabara@gmail.com>
2022-06-24 05:50:38 -0700

    using argument count for example

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: arg count
+ * @argv: vector of strings with arguments
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+}
