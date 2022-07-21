05f34ae078e6ad65c456e272cf4ed938ac11425f
Ugoo <ugonnaezeabara@gmail.com>
2022-07-21 09:03:19 -0700

    learning bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,17 +1,46 @@
 #include "main.h"
 
 /**
+ * _pow_recursion - function that returns the value of x
+ * raised to the power of y
+ * @x: base number
+ * @y: pow number
+ * Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
+
+/**
  * print_binary - function that prints the binary representation of a number
  * @n: number
 */
 
 void print_binary(unsigned long int n)
 {
-	int binary;
+	unsigned int res_pow = 0;
+	int exp = 10;
+	int flag = 0;
 
-	if (n >> 1)
-		print_binary(n >> 1);
-	/* get the last bit of the number */
-	binary = n & 1;
-	putchar(binary + '0');
+	if (n == 0)
+		_putchar('0');
+	while (exp >= 0)
+	{
+		res_pow = _pow_recursion(2, exp);
+		if (n >= res_pow)
+		{
+			_putchar('1');
+			n -= res_pow;
+			flag = 1;
+		}
+		else if (n < res_pow && flag == 1)
+			_putchar('0');
+		exp--;
+	}
 }
