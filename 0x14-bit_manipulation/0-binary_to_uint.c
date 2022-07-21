69ad2756a18ae5ee44b7bb2a23cacff3d0db34ff
Ugoo <ugonnaezeabara@gmail.com>
2022-07-21 08:45:54 -0700

    learning bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,13 +9,9 @@ void print_binary(unsigned long int n)
 {
 	int binary;
 
-	while (binary >= 0)
-	{
-		if ((n >> binary) & 1)
-			_putchar('1');
-		else
-			_putchar('0');
-
-		binary--;
-	}
+	if (n >> 1)
+		print_binary(n >> 1);
+	/* get the last bit of the number */
+	binary = n & 1;
+	putchar(binary + '0');
 }
